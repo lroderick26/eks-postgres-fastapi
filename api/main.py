9444a1c17a1c17c4de9f3b98641e6790e569ab40
lroderick26 <31170856+lroderick26@users.noreply.github.com>
2023-05-26 14:33:17 -0700

    Pushing to check data in db

--- api/main.py
+++ api/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine
+from pydantic import BaseModel, Field
 
 app = FastAPI()
-
+engine = create_engine(POSTGRES_URI)
+conn = engine.connect()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/data")
+def get_data():
+    sql_statement = "SELECT * FROM lwtdemo.script_records"
+    returned_data = conn.execute(sql_statement).fetchall()
+    return returned_data
