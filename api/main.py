e743bdb71e55851605ba44e2ab82c7c372c06b20
lroderick26 <31170856+lroderick26@users.noreply.github.com>
2023-05-26 15:56:48 -0700

    Adding in separate db file

--- api/main.py
+++ api/main.py
@@ -1,20 +1,40 @@
-from fastapi import FastAPI
 from sqlalchemy import create_engine, text
-from sqlalchemy.ext.asyncio import create_async_engine
-import os
+from typing import List
+
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from starlette.responses import RedirectResponse
+from .db import SessionLocal, engine
 
 app = FastAPI()
-engine = create_async_engine(os.getenv("POSTGRES_URI"))
-# conn = engine.connect()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def main():
+    return RedirectResponse(url="/docs/")
 
 
 @app.get("/data")
-async def get_data():
-    async with engine.connect() as conn:
-        sql_statement = text("""SELECT * FROM lwtdemo.script_records;""")
-        result = await conn.execute(sql_statement)
-    return result
+def show_records(db: Session = Depends(get_db)):
+    sql_statement = text("""SELECT * FROM lwtdemo.script_records;""")
+    records = db.query(sql_statement).all()
+    return records
